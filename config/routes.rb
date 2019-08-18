9179e6add40b91deb0ea76171d11ee4342894aa0
Michael JunDundas <umjundun@gmail.com>
2019-08-18 15:38:42 -0400

    added autentication with different homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  devise_scope :user do
+    authenticated  do
+      root to: 'pages#home'
+    end
+
+    unauthenticated do
+      root to: 'devise/registrations#new'
+    end
+  end
+  # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
